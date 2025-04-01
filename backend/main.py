e16fb28b2a0c3883b2b9940b146d5b2d3d62d04a
devlepa <juanestebanleonparra2@gmail.com>
2025-04-01 15:46:53 -0500

    :)

--- backend/main.py
+++ backend/main.py
@@ -11,15 +11,10 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-origins = [
-    "http://localhost:5173",
-    "http://ec2-3-91-179-100.compute-1.amazonaws.com:5173",
-    "http://ec2-3-91-179-100.compute-1.amazonaws.com",
-]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
