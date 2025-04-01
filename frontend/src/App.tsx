7161b2faaec195a05b85fd3341dc06bd78cb97d5
devlepa <juanestebanleonparra2@gmail.com>
2025-04-01 13:01:42 -0500

    _)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ActorsList from './components/ActorsList';
 import AddActorForm from './components/AddActorForm';
+import './styles.css';
 
 const App = () => {
+    const [refresh, setRefresh] = useState(0);
+
+    const handleRefresh = () => {
+        setRefresh(refresh + 1);
+    };
+
     return (
-        <div className="App">
+        <div className="container">
             <h1>Actors Management</h1>
-            <AddActorForm />
-            <ActorsList />
+            <AddActorForm onRefresh={handleRefresh} />  {/* Cambiado aquí */}
+            <ActorsList onRefresh={refresh} />
         </div>
     );
 };
