3c55f591d609ba8fa5bc9b7ca9200d1c5b3ff103
devlepa <juanestebanleonparra2@gmail.com>
2025-04-02 12:11:27 -0500

    .

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 from datetime import datetime
+from typing import Optional
 
 class ActorBase(BaseModel):
     first_name: str
@@ -14,3 +15,14 @@ class ActorOut(ActorBase):
 
     class Config:
         orm_mode = True
+    
+class FilmAvailability(BaseModel):
+    film_id: int
+    title: str
+    inventory_id: int
+    store_id: int
+    store_location: Optional[str] = None
+    is_rented: bool
+
+    class Config:
+        from_attributes = True  # Pydantic v2 - Reemplazo de 'orm_mode'
