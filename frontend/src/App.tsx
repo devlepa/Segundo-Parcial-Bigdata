839d287b207b74917c4414dfe95cfaa3dd16bfb0
devlepa <juanestebanleonparra2@gmail.com>
2025-04-04 11:44:10 -0500

    añadiendo imagenes y ajustando ancho

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Layout from "./components/Layout";
 import RentMovies from "./pages/RentMovies";
 import Actors from "./pages/Actors";
@@ -10,15 +15,42 @@ import "./styles.css";
 const App: React.FC = () => {
   return (
     <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<RentMovies />} />{" "}
-          {/* Root points to RentMovies */}
-          <Route path="/actors" element={<Actors />} />
-          <Route path="/film-availability" element={<FilmAvailability />} />
-          <Route path="/search-movies" element={<SearchMovies />} />
-        </Routes>
-      </Layout>
+      <Routes>
+        {/* Redirect root path to /rent-movies */}
+        <Route path="/" element={<Navigate to="/rent-movies" replace />} />
+        <Route
+          path="/rent-movies"
+          element={
+            <Layout>
+              <RentMovies />
+            </Layout>
+          }
+        />
+        <Route
+          path="/actors"
+          element={
+            <Layout>
+              <Actors />
+            </Layout>
+          }
+        />
+        <Route
+          path="/film-availability"
+          element={
+            <Layout>
+              <FilmAvailability />
+            </Layout>
+          }
+        />
+        <Route
+          path="/search-movies"
+          element={
+            <Layout>
+              <SearchMovies />
+            </Layout>
+          }
+        />
+      </Routes>
     </Router>
   );
 };
