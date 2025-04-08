2dbb693db7fa7f62f51e16bf6d576ea8f71dfaba
devlepa <juanestebanleonparra2@gmail.com>
2025-04-08 10:01:52 -0500

    .

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -4,10 +4,10 @@ import Footer from "./Footer";
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
-    <div className="bg-gradient-to-b from-black via-gray-900 to-black min-h-screen flex flex-col justify-between">
+    <div className="bg-gradient-to-b from-[#ED6A5A] via-[#F4F1BB] to-[#9BC1BC] min-h-screen flex flex-col justify-between">
       <Navbar />
       <main className="flex-grow flex justify-center items-center w-full">
-        <div className="container w-full max-w-4xl bg-gray-800 p-8 rounded-lg shadow-2xl text-center text-white">
+        <div className="container w-full max-w-4xl bg-[#E6EBE0] p-8 rounded-lg shadow-2xl text-center text-[#363636]">
           {children}
         </div>
       </main>
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -3,16 +3,16 @@ import { Link } from "react-router-dom";
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="bg-black shadow-lg fixed top-0 left-0 w-full z-50">
+    <nav className="bg-[#36C9C6] shadow-lg fixed top-0 left-0 w-full z-50">
       <div className="container mx-auto px-4 py-3 flex justify-between items-center">
-        <Link to="/" className="text-red-600 text-2xl font-bold">
-          NetflixClone
+        <Link to="/" className="text-[#ED6A5A] text-2xl font-bold">
+          MyMovies
         </Link>
         <ul className="flex space-x-6">
           <li>
             <Link
               to="/rent-movies"
-              className="text-white hover:text-red-600 transition duration-300"
+              className="text-[#363636] hover:text-[#ED6A5A] transition duration-300"
             >
               Alquilar
             </Link>
@@ -20,7 +20,7 @@ const Navbar: React.FC = () => {
           <li>
             <Link
               to="/film-availability"
-              className="text-white hover:text-red-600 transition duration-300"
+              className="text-[#363636] hover:text-[#ED6A5A] transition duration-300"
             >
               Disponibilidad
             </Link>
@@ -28,7 +28,7 @@ const Navbar: React.FC = () => {
           <li>
             <Link
               to="/search-movies"
-              className="text-white hover:text-red-600 transition duration-300"
+              className="text-[#363636] hover:text-[#ED6A5A] transition duration-300"
             >
               Buscar
             </Link>
@@ -36,7 +36,7 @@ const Navbar: React.FC = () => {
           <li>
             <Link
               to="/actors"
-              className="text-white hover:text-red-600 transition duration-300"
+              className="text-[#363636] hover:text-[#ED6A5A] transition duration-300"
             >
               Actores
             </Link>
